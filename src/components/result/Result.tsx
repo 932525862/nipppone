2d925cae05e865a642bb6a7363d7cf5909c4a352
Otabek <turayevotabek.0002@gmail.com>
2024-08-20 13:13:43 +0500

    last update

--- src/components/result/Result.tsx
+++ src/components/result/Result.tsx
@@ -4,6 +4,7 @@ import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import studentImg from "../../assets/student-certificate.png";
+import Marquee from "react-fast-marquee";
 
 export const Result = () => {
   return (
@@ -17,176 +18,61 @@ export const Result = () => {
       </div>
       <div className="result-swiper">
         <div className="swiper-top">
-          <Swiper
-            slidesPerView={1}
-            spaceBetween={20}
-            breakpoints={{
-              662: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              950: {
-                slidesPerView: 3,
-                spaceBetween: 40,
-              },
-              1300: {
-                slidesPerView: 4,
-                spaceBetween: 20,
-              },
-            }}
-            autoplay={{
-              delay: 1000,
-              disableOnInteraction: false,
-              reverseDirection: true
-            }}
-            mousewheel={
-              {invert: true}
-            }
-            speed={2000}
-            loop={true}
-            modules={[Autoplay]}
-            
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-          </Swiper>
+          <Marquee direction="right">
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+          </Marquee>
         </div>
         <div className="swiper-bottom">
-          <Swiper
-            slidesPerView={3}
-            spaceBetween={20}
-            breakpoints={{
-              768: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-              },
-              1300: {
-                slidesPerView: 4,
-                spaceBetween: 20,
-              },
-            }}
-            autoplay={{
-              delay: 1000,
-              disableOnInteraction: false,
-            }}
-            speed={2000}
-            loop={true}
-            modules={[Autoplay]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={studentImg}
-                alt="photo"
-              />
-            </SwiperSlide>
-          </Swiper>
+          <Marquee>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+            <div>
+              <img src={studentImg} alt="photo" />
+            </div>
+          </Marquee>
         </div>
       </div>
       <div className="container">
         <div className="result-content d-flex justify-center">
-          <a href="#support"><HeroButton text="ariza qoldirish"></HeroButton></a>
+          <a href="#support">
+            <HeroButton text="ariza qoldirish"></HeroButton>
+          </a>
         </div>
       </div>
     </section>
