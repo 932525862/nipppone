2d0f6bdb738cd94bd1b655fc87038841da70b75f
Otabek <turayevotabek195@gmail.com>
2024-10-16 17:48:58 +0500

    fix: SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,62 @@
-
-import { useState } from 'react'
-import './App.css'
-import { About } from './components/about/About'
-import { Academy } from './components/academy/Academy'
-import { Course } from './components/course/Course'
-import Faq from './components/faq/Faq'
-import { Footer } from './components/footer/Footer'
-import { JLPT } from './components/jlpt/Jlpt'
-import { Header } from './components/header/Header'
-import { Hero } from './components/hero/Hero'
-import ModalCard from './components/modal/Modal'
-import { Result } from './components/result/Result'
-import { University } from './components/university/University'
-import { Support } from './components/support/Support'
-import { ToastContainer} from 'react-toastify';
-import News from './components/news/News'
-import ResultsPupil from './components/resultsPupil/ResultsPupil'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { About } from "./components/about/About";
+import { Academy } from "./components/academy/Academy";
+import { Course } from "./components/course/Course";
+import Faq from "./components/faq/Faq";
+import { Footer } from "./components/footer/Footer";
+import { JLPT } from "./components/jlpt/Jlpt";
+import { Header } from "./components/header/Header";
+import { Hero } from "./components/hero/Hero";
+import ModalCard from "./components/modal/Modal";
+import { Result } from "./components/result/Result";
+import { University } from "./components/university/University";
+import { Support } from "./components/support/Support";
+import { ToastContainer } from "react-toastify";
+import News from "./components/news/News";
+import ResultsPupil from "./components/resultsPupil/ResultsPupil";
+import Loader from "./components/loader/Loader";
 
 function App() {
-  const [open, setOpen] = useState<boolean>(false)
+  const [open, setOpen] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  }, []);
 
   return (
     <>
-    <div className='bg-body'></div>
-      <ToastContainer />
-      <ModalCard open={open} setOpen={setOpen}/>
-      <div className="wrapper">
-        <Header/>
-        <main>
-          <Hero/>
-          <News/>
-          <About/>
-          <Result/>
-          <JLPT/>
-          <ResultsPupil/>
-          <University setOpen={setOpen}/>
-          <Course setOpen={setOpen}/>
-          <Academy/>
-          <Faq/>
-          <Support/>
-        </main>
-        <Footer/>
-      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="bg-body"></div>
+          <ToastContainer />
+          <ModalCard open={open} setOpen={setOpen} />
+          <div className="wrapper">
+            <Header />
+            <main>
+              <Hero />
+              <News />
+              <About />
+              <Result />
+              <JLPT />
+              <ResultsPupil />
+              <University setOpen={setOpen} />
+              <Course setOpen={setOpen} />
+              <Academy />
+              <Faq />
+              <Support />
+            </main>
+            <Footer />
+          </div>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
